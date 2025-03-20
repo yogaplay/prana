48e00e5f73623fce70d921972dcde50414e81fef
ebeleey <ebeleey@gmail.com>
2025-03-20 10:40:27 +0900

    [FE/FEAT] 초기화면 세팅 (#2)

--- frontend/lib/features/onboarding/pages/onboarding_page.dart
+++ frontend/lib/features/onboarding/pages/onboarding_page.dart
@@ -5,6 +5,45 @@ class OnboardingPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Color(0xFF7ECECA),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/images/logo.png'),
+            SizedBox(height: 20), // 이미지와 텍스트 사이 간격
+            Text(
+              '영혼을 정화하는 시간',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 200),
+            Text(
+              '아직 계정이 없으세요?',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 4),
+            GestureDetector(
+              onTap: () {
+                print('카카오로 시작하기 클릭');
+              },
+              child: Image.asset(
+                'assets/images/kakao_login_medium_narrow.png', 
+                height: 45,
+                fit: BoxFit.fitHeight,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
