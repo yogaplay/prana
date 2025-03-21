bffda3362f69b0513cf2e53a5db1b3f8cb74d6f5
ebeleey <ebeleey@gmail.com>
2025-03-21 18:03:18 +0900

    [FE/CONFIG] riverpod 설정 중간 커밋

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:frontend/app.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
@@ -9,5 +10,9 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   KakaoSdk.init(nativeAppKey: dotenv.env['KAKAO_NATIVE_APP_KEY'] ?? '');
 
-  runApp(const App());
+  runApp(
+    ProviderScope(
+      child: App(),
+    )
+  );
 }
