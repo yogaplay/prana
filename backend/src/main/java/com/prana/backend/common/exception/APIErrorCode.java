0af1d95184c7956e2aab06b076f98746b9ece723
이건우 <lkj2664@gmail.com>
2025-03-21 09:41:02 +0900

    [BE/FEAT] Exception 변경

--- backend/src/main/java/com/prana/backend/common/exception/APIErrorCode.java
+++ backend/src/main/java/com/prana/backend/common/exception/APIErrorCode.java
@@ -14,7 +14,6 @@ public enum APIErrorCode implements ErrorCode {
     INVALID_REFRESH_TOKEN(40111, "유효하지 않은 Refresh Token입니다."),
     INVALID_ACCESS_TOKEN(40112, "유효하지 않은 Access Token입니다."),
     UNREGISTERED_USER(40113, "등록되지 않은 사용자입니다."),
-    INVALID_KAKAO_ACCESS_TOKEN(40114, "유효하지 않은 Kakao Access Token입니다."),
 
     // 403 Forbidden
     FORBIDDEN(40300, "Forbidden"),
