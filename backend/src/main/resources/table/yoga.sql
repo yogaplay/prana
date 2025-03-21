887c5951765be1025c382a9a8daac37e81f455ad
이건우 <lkj2664@gmail.com>
2025-03-21 09:41:23 +0900

    [BE/FEAT] DB 테이블 컬럼 추가 (#4)

--- backend/src/main/resources/table/yoga.sql
+++ backend/src/main/resources/table/yoga.sql
@@ -4,7 +4,8 @@ CREATE TABLE yoga
     yoga_name     VARCHAR(255) NOT NULL,          -- yoga 이름
     video         VARCHAR(255) NOT NULL,          -- 영상 URL 또는 경로
     description   TEXT,                           -- 설명 (자세한 설명을 담기 위해 TEXT 사용)
-    solution_pose TEXT         NOT NULL           -- 해결책 (자세한 해결책을 담기 위해 TEXT 사용)
+    solution_pose TEXT         NOT NULL,          -- 해결책 (자세한 해결책을 담기 위해 TEXT 사용)
+    image         VARCHAR(255) NOT NULL           -- yoga 이미지
 );
 
 
