8b568b2fecdbfb0c803ec67b8ba0c11abc133c73
임혁 <cs.javah@kakao.com>
2025-03-25 12:52:08 +0900

    [BE/FEAT] 즐겨찾기 (#15)

--- backend/src/main/java/com/prana/backend/home/controller/HomeController.java
+++ backend/src/main/java/com/prana/backend/home/controller/HomeController.java
@@ -1,14 +1,18 @@
 package com.prana.backend.home.controller;
 
 import com.prana.backend.common.PranaPrincipal;
+import com.prana.backend.home.controller.request.PagedStarRequest;
+import com.prana.backend.home.controller.request.StaringRequest;
 import com.prana.backend.home.controller.response.HomeResponse;
+import com.prana.backend.home.controller.response.PagedResponse;
+import com.prana.backend.home.controller.response.StarResponse;
+import com.prana.backend.home.controller.response.StaringResponse;
 import com.prana.backend.home.service.HomeService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/home")
@@ -21,4 +25,15 @@ public class HomeController {
     public ResponseEntity<HomeResponse> home(@AuthenticationPrincipal PranaPrincipal pranaPrincipal) {
         return homeService.home(pranaPrincipal.getUserId());
     }
+
+    @GetMapping("/star")
+    public ResponseEntity<PagedResponse<StarResponse>> pagedStar(@Valid PagedStarRequest request, @AuthenticationPrincipal PranaPrincipal pranaPrincipal) {
+        return homeService.pagedStar(request, pranaPrincipal.getUserId());
+    }
+
+    @PostMapping("/star")
+    public ResponseEntity<StaringResponse> staring(@Valid @RequestBody StaringRequest request, @AuthenticationPrincipal PranaPrincipal pranaPrincipal) {
+        return homeService.staring(request, pranaPrincipal.getUserId());
+    }
+
 }
---
+++ backend/src/main/java/com/prana/backend/home/controller/request/StaringRequest.java
@@ -0,0 +1,16 @@
+package com.prana.backend.home.controller.request;
+
+import jakarta.validation.constraints.Min;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StaringRequest {
+    @Min(1)
+    private int sequenceId;
+}
