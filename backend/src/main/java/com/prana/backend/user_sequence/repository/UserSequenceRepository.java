59cf8b6dbbb27a2804dcb78b5ca68c48927bc2f9
이건우 <lkj2664@gmail.com>
2025-03-25 13:13:14 +0900

    [BE/FEAT] 요가 수행 시, User 스트릭 갱신 (#16)

--- backend/src/main/java/com/prana/backend/user_sequence/repository/UserSequenceRepository.java
+++ backend/src/main/java/com/prana/backend/user_sequence/repository/UserSequenceRepository.java
@@ -2,6 +2,14 @@ package com.prana.backend.user_sequence.repository;
 
 import com.prana.backend.entity.UserSequence;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserSequenceRepository extends JpaRepository<UserSequence, Integer> {
+
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM user_sequence WHERE DATE(updated_at) = CURDATE() - INTERVAL 1 DAY)", nativeQuery = true)
+    boolean existsYesterdayUserSequence();
+
+    @Query(value = "SELECT EXISTS(SELECT 1 FROM user_sequence WHERE DATE(updated_at) = CURDATE())", nativeQuery = true)
+    boolean existsTodayUserSequence();
+
 }
