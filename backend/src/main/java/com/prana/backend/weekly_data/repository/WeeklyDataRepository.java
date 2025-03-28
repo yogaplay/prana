1b9b6f01b03e9e16bf83ae5b078cda03c783f567
seulgi980 <seulgi980@naver.com>
2025-03-28 10:49:13 +0900

    [BE/FEAT] 주간 리포트 데이터 저장 스케쥴러 1주전 데이터 조회 메서드 생성

--- backend/src/main/java/com/prana/backend/weekly_data/repository/WeeklyDataRepository.java
+++ backend/src/main/java/com/prana/backend/weekly_data/repository/WeeklyDataRepository.java
@@ -2,7 +2,24 @@ package com.prana.backend.weekly_data.repository;
 
 import com.prana.backend.entity.WeeklyData;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface WeeklyDataReposiory extends JpaRepository<WeeklyData, Long> {
+import java.util.List;
+
+public interface WeeklyDataRepository extends JpaRepository<WeeklyData, Long> {
     WeeklyData findWeeklyDataByUserIdAndYearAndMonthAndWeek(int userId, int year, int month, int week);
+
+    @Query("""
+    SELECT wd
+    FROM WeeklyData wd
+    WHERE wd.year = :year AND wd.month = :month AND wd.week = :week
+    AND wd.user.id IN :userIds
+""")
+    List<WeeklyData> findAllByYearMonthWeekAndUserIdIn(
+            @Param("year") int year,
+            @Param("month") int month,
+            @Param("week") int week,
+            @Param("userIds") List<Integer> userIds
+    );
 }
