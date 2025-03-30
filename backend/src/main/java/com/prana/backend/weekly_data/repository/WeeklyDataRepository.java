b77fd7f1af7c8c9dd3585845a7c56c57f96b40b0
seulgi980 <seulgi980@naver.com>
2025-03-30 18:42:07 +0900

    [BE/FEAT] WeeklyData가 비어있을 경우를 방지해 Optional로 변경

--- backend/src/main/java/com/prana/backend/weekly_data/repository/WeeklyDataRepository.java
+++ backend/src/main/java/com/prana/backend/weekly_data/repository/WeeklyDataRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface WeeklyDataRepository extends JpaRepository<WeeklyData, Long> {
-    WeeklyData findWeeklyDataByUserIdAndYearAndMonthAndWeek(int userId, int year, int month, int week);
+    Optional<WeeklyData> findWeeklyDataByUserIdAndYearAndMonthAndWeek(int userId, int year, int month, int week);
 
     @Query("""
     SELECT wd
