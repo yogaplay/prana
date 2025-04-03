fc0c4e978ce03e4e763d5dc9b0c03e8b385f46f3
seulgi980 <seulgi980@naver.com>
2025-04-03 10:57:08 +0900

    [BE/FEAT] 특정 tag에 해당되는 sequence정보 조회 API개발

--- backend/src/main/java/com/prana/backend/sequence/repository/SequenceRepository.java
+++ backend/src/main/java/com/prana/backend/sequence/repository/SequenceRepository.java
@@ -18,4 +18,14 @@ public interface SequenceRepository extends JpaRepository<Sequence, Integer> {
         LIMIT 3
         """, nativeQuery = true)
     List<Sequence> findRandom3ByTagName(@Param("tagName") String tagName);
+
+    @Query(value = """
+        SELECT s.sequence_id, s.sequence_name, s.image, GROUP_CONCAT(DISTINCT t.tag_name)
+        FROM sequence s
+        JOIN sequence_tag st ON st.sequence_id = s.sequence_id
+        JOIN tag t ON st.tag_id = t.tag_id
+        GROUP BY s.sequence_id, s.sequence_name, s.image
+        HAVING FIND_IN_SET(:tagName, GROUP_CONCAT(DISTINCT t.tag_name)) > 0
+""", nativeQuery = true)
+    List<Object[]> findByTag(@Param("tagName") String tagName);
 }
