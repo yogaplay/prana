4dd41097551af2dee267ec0f703beec8faed6491
ebeleey <ebeleey@gmail.com>
2025-04-03 22:13:19 +0900

    [FE/FEAT] API 요청 함수 작성 (#44)

--- frontend/lib/features/learning/services/learning_service.dart
+++ frontend/lib/features/learning/services/learning_service.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:dio/dio.dart';
 import 'package:frontend/core/api/api_client.dart';
 
 class LearningService {
@@ -5,12 +8,62 @@ class LearningService {
 
   LearningService(this._apiClient);
 
-  Future startYoga(int sequenceId) async {
+  //시퀀스 시작 시, userSequence 생성
+  Future<int> startYoga(int sequenceId) async {
     final response = await _apiClient.post(
       '/yoga/start',
       body: {'sequenceId': sequenceId},
     );
-  return response['userSequenceId'];
+    return response['userSequenceId'];
   }
-  
+
+  // 시퀀스 중 요가 시작 시, 해당 포인트를 저장
+  Future<void> saveYogaPose(int userSequenceId) async {
+    await _apiClient.post(
+      '/yoga/pose',
+      body: {'userSequenceId': userSequenceId},
+    );
+  }
+
+  // 매 3초마다 피드백을 위한 API
+  Future<String> sendLongFeedback({
+    required int yogaId,
+    required int userSequenceId,
+    required File imageFile,
+  }) async {
+    final response = await _apiClient.postMultipart(
+      '/feedback/long',
+      fields: {'yogaId': yogaId, 'userSequenceId': userSequenceId},
+      files: {'image': imageFile},
+    );
+    return response['data'];
+  }
+
+  // 매 0.5초마다 성공, 실패 여부를 판단하기 위한 API
+  Future<String> sendShortFeedback({
+    required int yogaId,
+    required int userSequenceiD,
+    required File imageFile,
+  }) async {
+    final response = await _apiClient.postMultipart(
+      '/feedback/short',
+      fields: {'yogaId': yogaId, 'userSequenceId': userSequenceiD},
+      files: {'image': imageFile},
+    );
+    return response['data'];
+  }
+
+  // 시퀀스 진행 중, 요가 동작이 끝날때마다 피드백 저장  
+  Future<void> saveAccuracyComplete({
+    required int userSequenceId,
+    required int yogaId,
+    required int sequenceId 
+  }) async {
+    await _apiClient.post('/accuracy/complete', body:{
+      'userSequenceId': userSequenceId,
+      'yogaId': yogaId,
+      'sequenceId': sequenceId
+    });
+    
+  }
 }
