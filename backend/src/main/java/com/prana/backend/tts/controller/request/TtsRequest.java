6896612e56b1cb4ea19a359b15b9a7d6025846dc
임혁 <cs.javah@kakao.com>
2025-04-04 13:44:09 +0900

    [BE/FEAT] TTS 제공 API

---
+++ backend/src/main/java/com/prana/backend/tts/controller/request/TtsRequest.java
@@ -0,0 +1,16 @@
+package com.prana.backend.tts.controller.request;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class TtsRequest {
+    @NotBlank
+    private String text;
+}
